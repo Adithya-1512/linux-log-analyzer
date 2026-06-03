29bb729970f2b65884ccb5402853e175f4ddc076
Adithya-1512 <adithyadevopser15@gmail.com>
2026-06-03 03:02:12 +0000

    Added top error analysis

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Linux Log Analyzer"
+echo "===== Linux Log Analyzer ====="
+
+LOGFILE="/var/log/syslog"
+
+echo ""
+echo "Total Error Messages:"
+grep -i "error" $LOGFILE | wc -l
+
+echo ""
+echo "Total Warning Messages:"
+grep -i "warning" $LOGFILE | wc -l
+
+echo ""
+echo "Top 10 Error Messages:"
+grep -i "error" $LOGFILE | sort | uniq -c | sort -nr | head -10
